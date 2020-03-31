667f99275be3eec6a74929fa1b072d1d62f6a332
rafaelwei <rafaelwei07@gmail.com>
2020-03-31 14:53:06 -0300

    First commit

---
+++ moradiaCompartilhada/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  moradiaCompartilhada
+//
+//  Created by Rafael Wei on 31/03/20.
+//  Copyright © 2020 Rafael Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
