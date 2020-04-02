ce262159a7c55b02755c7b8d45446e9144e60fab
Kassio Veras <multiverssio@gmail.com>
2020-04-02 18:38:54 -0300

    pequenas alterações na tela de login e busca

--- moradiaCompartilhada/ViewController.swift
+++ moradiaCompartilhada/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tfEmail(_ sender: Any) {
+    }
+    @IBAction func tfSenha(_ sender: Any) {
+    }
+    
     @IBAction func btnLogin(_ sender: Any) {
     }
     
