4f777905d517b3d559d2c18e469aec85df1eef31
Victor <victorduarte.contato@gmail.com>
2020-04-07 14:22:59 -0300

    Nova tentativa de Push.

---
+++ moradiaCompartilhada/viewProfileEditing.swift
@@ -0,0 +1,43 @@
+//
+//  viewProfileEditing.swift
+//  moradiaCompartilhada
+//
+//  Created by Victor Duarte on 07/04/20.
+//  Copyright © 2020 Rafael Wei. All rights reserved.
+//
+
+import UIKit
+
+
+class viewProfileEditing: UIViewController {
+    
+    
+    @IBAction func tfUserName(_ sender: Any) {
+    }
+    @IBAction func tfAge(_ sender: Any) {
+    }
+    @IBAction func tfOccupation(_ sender: Any) {
+    }
+    @IBAction func tfOccupationLocal(_ sender: Any) {
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
